f80b5856ec45a3a36ba6c21d09ea5e34b6de52f8
wangsimin <taoyiran815579058@outlook.com>
2021-05-03 05:40:32 +0800

    1st

--- src/test/java/com/xxx/grad/utils/TimeUtilsTest.java
+++ src/test/java/com/xxx/grad/utils/TimeUtilsTest.java
@@ -8,8 +8,8 @@ public class TimeUtilsTest {
     public void test() {
         //System.out.println(TimeUtils.getCurTime());
         Long currentTime = TimeUtils.getCurrentTime();
-        System.out.println(TimeUtils.millToDate(1619986234633l));
-        System.out.println(TimeUtils.millToDate(1619986234635l));
+        System.out.println(TimeUtils.millToDate(1619991536495l));
+        //System.out.println(TimeUtils.millToDate(1619986234635l));
         System.out.println();
 
         //System.out.println(TimeUtils.dateToMills("2021/05/01"));
